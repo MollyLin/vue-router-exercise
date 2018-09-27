4a37cd8de681f9d50de19cba739133f478c97ad5
Molly_Lin <mollylin2014@gmail.com>
2018-09-27 14:41:52 +0800

    製作巢狀路由頁面

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,9 @@ import VueRouter from 'vue-router';
 // 自訂元件
 import Home from '@/components/HelloWorld';
 import Page from '@/components/pages/page';
+import child from '@/components/pages/child';
+import child2 from '@/components/pages/child2';
+import child3 from '@/components/pages/child3';
 
 // 啟用
 Vue.use(VueRouter);
@@ -19,5 +22,21 @@ export default new VueRouter({
         name: '頁面',
         path: '/page',
         component: Page,
+        children: [{
+                name: 'Card 1',
+                path: '',
+                component: child
+            },
+            {
+                name: 'Card 2',
+                path: 'child2',
+                component: child2
+            },
+            {
+                name: 'Card 3',
+                path: 'child3',
+                component: child3
+            }
+        ]
     }]
 });
